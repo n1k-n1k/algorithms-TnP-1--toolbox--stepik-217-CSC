6909e667bdcbf11317fd447da0819f581996251f
Nik Brezginov <n1k-n1k@inbox.ru>
2020-03-12 21:35:22 +0300

    priority queue v2
    @ minHeap

--- 02_greedy_algs/02_5_priority_queue.py
+++ 02_greedy_algs/02_5_priority_queue.py
@@ -31,8 +31,80 @@ class PriorityQueueList:
         self.lst.sort()
 
 
+class PriorityQueueHeap:
+    def __init__(self, heap=None):
+        self._heap = heap or []
+
+    @staticmethod
+    def _parent_i(i):
+        if i:
+            return (i - 1) // 2
+
+    def _left_i(self, i):
+        _left = i * 2 + 1
+        if _left <= len(self._heap) - 1:
+            return _left
+
+    def _right_i(self, i):
+        _right = i * 2 + 2
+        if _right <= len(self._heap) - 1:
+            return _right
+
+    def _min_child_i(self, i):
+        _right_ind = self._right_i(i)
+        _left_ind = self._left_i(i)
+        _right = self._heap(_right_ind)
+        _left = self._heap(_left_ind)
+
+        if not (_right and _left):
+            return None
+        if not _right or _right > _left:
+            return _left_ind
+        else:
+            return _right_ind
+
+    def _swap(self, i, j):
+        self._heap[i], self._heap[j] = self._heap[j], self._heap[i]
+
+    def _move_up(self, i):
+        while i > 0:
+            if self._heap[i] < self._heap[self._parent_i(i)]:
+                self._swap(i, self._parent_i(i))
+                i = self._parent_i(i)
+            else:
+                return
+
+    def _move_down(self, i):
+        while self._min_child_i(i):
+            tmp = i
+            min_child_tmp = self._min_child_i(tmp)
+            self._swap(tmp, min_child_tmp)
+            i = min_child_tmp
+
+    def get_max(self):
+        return self._heap[-1]
+
+    def extract_max(self):
+        if len(self._heap):
+            return self._heap.pop()
+
+    def get_min(self):
+        return self._heap[0]
+
+    def extract_min(self):
+        if len(self._heap):
+            root = self._heap[0]
+            self._heap[0] = self._heap.pop()
+            self._move_down(0)
+            return root
+
+    def insert(self, n):
+        self._heap.append(n)
+        self._move_up(len(self._heap) - 1)
+
+
 def parse_input():
-    q = PriorityQueueList()
+    q = PriorityQueueHeap()
 
     for _ in range(int(input())):
         cmd = input()
