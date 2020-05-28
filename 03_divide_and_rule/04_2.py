b2f3129cbe92793173b4f6d664354210d10a597a
Nik Brezginov <n1k-n1k@inbox.ru>
2020-05-28 22:10:10 +0300

    Точки и отрезки, v1.1
    custom quick_sort-1

--- 03_divide_and_rule/04_2.py
+++ 03_divide_and_rule/04_2.py
@@ -13,28 +13,68 @@
 Для каждой точки в порядке появления во вводе выведите, скольким отрезкам она принадлежит.
 """
 
+
+def quick_sort_mod(lst, start, end):
+    if end - start > 1:
+        pivot = partition(lst, start, end)
+        quick_sort_mod(lst, start, pivot)
+        quick_sort_mod(lst, pivot + 1, end)
+    return lst
+
+
+def partition(lst, start, end):
+    pivot = lst[start]
+    i = start + 1
+    j = end - 1
+
+    while True:
+        while i <= j and lst[i] <= pivot:
+            i = i + 1
+        while i <= j and lst[j] >= pivot:
+            j = j - 1
+
+        if i <= j:
+            lst[i], lst[j] = lst[j], lst[i]
+        else:
+            lst[start], lst[j] = lst[j], lst[start]
+            return j
+
+
 # testdada
 # n, m = 6, 6
-# segments = [[0, 31], [1, 3], [2, 3], [3, 4], [3, 5], [3, 6]]
+# starts = [0, 1, 2, 3, 3, 3]
+# stops = [31, 3, 3, 4, 5, 6]
 # points = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
 
 n, m = map(int, input().split())
-segments = [list(map(int, input().split())) for _ in range(n)]
+
+starts = []
+stops = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    starts.append(a)
+    stops.append(b)
+
 points = {int(i): 0 for i in input().split()}
 
-starts = sorted(segments)
-ends = sorted(segments, key=lambda x: x[1])
+starts = quick_sort_mod(starts, 0, n)
+ends = quick_sort_mod(stops, 0, n)
 
 for p in points:
     start_cnt, end_cnt = 0, 0
     for s in starts:
-        if s[0] <= p:
+        if s <= p:
             start_cnt += 1
+        else:
+            break
 
     for e in ends:
-        if e[1] < p:
+        if e < p:
             end_cnt += 1
+        else:
+            break
 
     points[p] = start_cnt - end_cnt
 
